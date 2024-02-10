d938263a7a4b9273fc879cd5fe8e4ebac78359fb
Gustavo <gs.cesario1998@gmail.com>
2024-02-09 22:15:30 -0300

    backup formatação

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,57 @@ const inicializar = async () => {
   iniciarFuncionalidades();
 };
 
+const list = [
+  {
+    title: "Pasta",
+    rows: [
+      {
+        title: "Ravioli Lasagna",
+        description: "Made with layers of frozen cheese",
+      },
+    ],
+  },
+  {
+    title: "Dessert",
+    rows: [
+      {
+        title: "Baked Ricotta Cake",
+        description: "Sweets pecan baklava rolls",
+      },
+      {
+        title: "Lemon Meringue Pie",
+        description: "Pastry filled with lemonand meringue.",
+      },
+    ],
+  },
+];
+
 const iniciarFuncionalidades = () => {
-  client.onMessage(messageHandler);
+  //client.onMessage(messageHandler);
+  client.onMessage(async (msg) => {
+    const buttons = [
+      {
+        buttonText: {
+          displayText: "Text of Button 1",
+          url: 'https://google.com'
+        },
+      },
+      {
+        buttonText: {
+          displayText: "Text of Button 2",
+          url: 'https://google.com'
+        },
+      },
+    ];
+    try {
+      await client.sendButtons(
+        msg.from,
+        "Title",
+        buttons,
+        "Description"
+      );
+    } catch (e) {console.log(e)}
+  });
 };
 
 inicializar();
